9146366569f8f74f2c5f052ce15203be017e61c1
naheem adedokun <nadedokun@heckerbella.com>
2022-11-15 16:18:00 +0100

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { envConfig } from '../config/constant/env.configuration';
+import { configValidation } from '../config/validator/config.validator';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>(envConfig.CONNECTION_STRING),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: false,
+      validationSchema: configValidation,
+      envFilePath: ['.env'],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
